c46f406d31297b152573ba05bd18782458b6f9d4
TJ Goze <tgoze@ntst.com>
2020-12-31 17:00:14 -0600

    Minimal changes to reproduce error with FAST Components.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import '@ntstforms/ntst-form-renderer/dist/main.js';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+import {FASTDesignSystemProvider, FASTRadio, FASTTextField} from '@microsoft/fast-components';
+
+// tslint:disable-next-line:no-unused-expression
+FASTDesignSystemProvider;
+// tslint:disable-next-line:no-unused-expression
+FASTRadio;
+// tslint:disable-next-line:no-unused-expression
+FASTTextField;
+
 
 if (environment.production) {
   enableProdMode();
